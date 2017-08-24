77ced03aa5abcb95b20ba9b7f6a4ebed4ac1caad
Balogun Abiodun <delighteddell@gmail.com>
2017-08-24 18:50:29 +0100

    setup client side development env

---
+++ client/webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const extractWebpackPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  devServer: {
+    contentBase: './dist/',
+    historyApiFallback: true
+  },
+  devtool: 'cheap-eval-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot|otf)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/',
+            publicPath: './'
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }],
+
+      },
+
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'images/',
+            publicPath: './'
+          }
+        }
+      },
+    ]
+  },
+  plugins: [
+    new extractWebpackPlugin('style.css'),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.$': 'jquery',
+      'window.jQuery': 'jquery'
+    })
+  ]
+};
+
+module.exports = config;
