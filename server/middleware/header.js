e171e82fd621fdbd268864ca5d6d5d38cd8d5f39
Balogun Abiodun <delighteddell@gmail.com>
2017-09-03 04:38:10 +0100

    refactor COR headers

--- server/middleware/header.js
+++ server/middleware/header.js
@@ -2,17 +2,19 @@
  * @class header
  * @classdesc sets header for Cross origin resouce sharing (CORS)
  */
-export default class Header {
+class Header {
   /**
    * @param {object} req
    * @param {object} res
    * @param {object} next
-   * @returns { object or a next function}
+   * @returns {object}
    */
-  static setHeaders(req, res, next) {
+  static setHeaders(res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, HEAD, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'X-PINGOTHER, Authorization, Origin, X-Requested-With, Content-Type, Accept');
     next();
   }
 }
+
+export default Header;
