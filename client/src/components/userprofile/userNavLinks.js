315291be5f761639b3e937f3c5c363ff9f0230b5
Balogun Abiodun <delighteddell@gmail.com>
2017-09-26 23:30:39 +0100

    modify user profile routes

--- client/src/components/userprofile/userNavLinks.js
+++ client/src/components/userprofile/userNavLinks.js
@@ -1,4 +1,7 @@
 export const userLinks = ['Dashboard' ,'My Profile' , 'Book in Library', 'History', 'Borrowed Books' , 'Notification'];
 export const userLinkIcons = ['dashboard' , 'person','library_books', 'history', 'book' , 'assistant_photo']
+export const userLinkText = ['dashboard' , 'profile', 'books', 'history', 'borrowedbooks' , 'notifications']
+
 export const adminLinks = ['Dashboard' , 'Books' ,'Upload Book', 'Notification' , 'Users', 'Add New Admin']
 export const adminLinkIcons = ['dashboard' , 'library_books', 'add_box' , 'assistant_photo', 'people', 'plus_one']
+export const adminLinkText = ['dashboard' , 'books', 'upload', 'notifications' , 'users', 'newAdmin']
