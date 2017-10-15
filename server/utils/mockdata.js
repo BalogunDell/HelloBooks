cfcf90cd008d5ab8229e65f5dec16de9ed4a38cf
Andela Developer <delighteddell@gmail.com>
2017-10-15 19:08:53 +0100

    refactor server-side tests

--- server/utils/mockdata.js
+++ server/utils/mockdata.js
@@ -2,29 +2,41 @@ const mockData = {
 
   // Valid user data
   user1: {
-    firstname: 'user1FirstName',
-    lastname: 'user1LastName',
-    username: 'user1Username',
-    email: 'user1Email@gmail.com',
-    password: 'user1password',
+    firstname: 'janet',
+    lastname: 'teddy',
+    email: 'jane5@mail.com',
+    username: 'abbey33',
+    password: 'abbey2',
     role: 'user',
-    membership: 'bronze',
-    image: 'image1'
+    membership: 'silver',
+    image: 'image2'
+  },
+
+  // Valid user data
+  userEdit: {
+    firstname: 'janet',
+    lastname: 'teddy',
+    username: 'abbey33'
+  },
+
+  borrowBookData: {
+    bookid: 2
   },
 
   adminData: {
     firstname: 'uadminFirstName',
     lastname: 'adminLastName',
-    username: 'user1Username',
+    username: 'userUsername123',
     email: 'adminEmail@gmail.com',
     password: 'adminpassword',
     role: 'admin',
-    membership: 'bronze'
+    membership: 'bronze',
+    image: 'image1'
   },
 
   adminSigninData: {
-    username: 'user1Username',
-    password: 'adminpassword',
+    username: 'adminUsername',
+    password: 'password',
   },
 
   invaliduser1Login: {
@@ -33,8 +45,8 @@ const mockData = {
   },
 
   user1Login: {
-    username: 'user1Username',
-    password: 'user1password',
+    username: 'abbey33',
+    password: 'abbey2',
   },
 
   user1ConflictData: {
@@ -49,7 +61,7 @@ const mockData = {
   user2: {
     firstname: 'user2FirstName',
     lastname: 'user2LastName',
-    username: 'user2Username',
+    username: 'userUsername123',
     email: 'user2Email@gmail.com',
     password: 'user2password',
     role: 'user',
@@ -60,14 +72,14 @@ const mockData = {
     firstname: 'user3FirstName',
     lastname: 'user3LastName',
     email: 'user3Email@gmail.com',
-    username: 'user3Username',
+    username: 'user1Username123',
     password: 'user3password',
     role: 'user',
     membership: 'gold'
   },
 
   adminLogin: {
-    username: 'adminUsername',
+    username: 'user1Username123',
     password: 'adminpassword'
   },
 
@@ -116,19 +128,36 @@ const mockData = {
   },
 
   bookdata: {
-    isbn: '#7764',
-    title: 'learn angular 4',
+    isbn: '1111',
+    title: 'React for Beginners',
     author: 'Nelson Brook',
-    pages: 123,
-    year: 1977,
+    pages: 1080,
+    year: 2010,
     description: 'the books does this and that',
-    quantity: 32,
-    category: 'Self Growth',
-    image: 'images/andela.jpg'
+    quantity: 3,
+    categoryid: 2,
+    visibility: true,
+    image: 'https://res.cloudinary.com/djvjxp2am/image/upload/v1507295978/book8_yy9efp.jpg',
+    pdf: 'https://res.cloudinary.com/djvjxp2am/image/upload/v1507295978/book8_yy9efp.jpg',
+  },
+
+  editBookdata: {
+    id: 2,
+    isbn: '4392',
+    title: 'React for Beginners',
+    author: 'Nelson Brook',
+    pages: 100,
+    year: 2000,
+    description: 'the books does this and that',
+    quantity: 300,
+    categoryid: 5,
+    visibility: true,
+    image: 'https://res.cloudinary.com/djvjxp2am/image/upload/v1507295978/book8_yy9efp.jpg',
+    pdf: 'https://res.cloudinary.com/djvjxp2am/image/upload/v1507295978/book8_yy9efp.jpg',
   },
 
   invalidBookdata: {
-    isbn: '#7764',
+    isbn: '7764',
     title: '',
     author: 'Nelson Brook',
     pages: 123,
@@ -195,11 +224,6 @@ const mockData = {
     quantity: 32,
     category: 'Self Growth',
     image: 'images/andela.jpg'
-  },
-
-  borrowBookData: {
-    userid: 1,
-    bookid: 2
   },
 
   userID: {
--- tests/api.test.js
+++ tests/api.test.js
@@ -11,279 +11,270 @@ const request = supertest(app);
 const secret = process.env.SECRET;
 let userToken;
 let adminToken;
+const api = '/api';
+const userAPI = '/api/users';
+let userId;
 
-/**
- * @desc describes and tests the home route
- * @return 200 succes
- */
+console.log('ENVIRONMENT:', process.env.NODE_ENV);
 
- describe('Homepage', () => {
+//**********************************//
+//********TEST THE HOME PAGE******* //
+//**********************************//
+
+describe('Homepage', () => {
   it('Should return welcome to libary api', (done) => {
     request
-      .get('/api/')
+      .get(api)
       .expect(200, done);
   });
 });
 
-/**
- * @desc describes and tests the signup api route
- * @return 201 succes
- */
-
-describe('User signup', (done) => {
-
-  // test for success with valid data for user
-  it('should be able to signup - User', (done) => {
+//**********************************//
+//********TEST GET BOOKS*********** //
+//**********************************//
+describe('Get books', () => {
+  it('should return list of books', (done) => {
     request
-    .post('/api/users/signup')
-    .set('Accept', 'Application/json')
-    .send(mockdata.user1)
-    .end((error, res) => {
-        expect(res.status).to.equal(201); 
-        done();
-      });
+    .get(`${api}/books/`)
+    .end((err, res, body) => {
+      expect(res.status).to.equal(200);
+      expect(res.body).to.have.key('books')
+      done();
     });
-
-    // test for success with valid data for admin
-  it('Should be able to signup - Admin', (done) => {
-    request
-    .post('/api/users/signup')
-    .set('Accept', 'Application/json')
-    .send(mockdata.adminData)
-    .end((error, res) => {
-        expect(res.status).to.equal(201); 
-        done();
-      }); 
-    });
-
-//  });
-// describe('User signin', (done) => {
-
-//   // test for user sign in
-//   it('Should be able to sign in and get a token - User', (done) => {
-//     request
-//     .post('/api/users/signin')
-//     .set('Accept', 'Application/json')
-//     .send(mockdata.user1Login)
-//     .end((error, res) =>{
-//       expect(res.status).to.equal(200);
-//       userToken = res.body.response.data.token;
-//       console.log(res.body);
-//     done();
-//     }); 
-// });
-
-
-
-// describe('User signin', (done) => {
-
-//   // test for user sign in
-//   it('should not allow login with invalid email', (done) => {
-//     request
-//     .post('/api/users/signin')
-//     .set('Accept', 'Application/json')
-//     .send(mockdata.invalidadmin)
-//       .end((error, res) => {
-//           expect({ message: 'Invalid email or password'});
-//           expect(401, done);
-//           done();
-//       }); 
-//     });
-
-//   // test for user sign in
-//   it('should not allow login with invalid email', (done) => {
-//     request
-//     .post('/api/users/signin')
-//     .set('Accept', 'Application/json')
-//     .send(mockdata.invaliduser)
-//       .end((error, res) => {
-//           expect({ message: 'Invalid email or password'});
-//           expect(401);
-//           done();
-//       }); 
-//     });
-
-// });
-
-// describe('Wrong User Credentials', (done) => {
-
-//   it('Should not be able to sign in - User', (done) => {
-//     request
-//     .post('/api/users/signin')
-//     .set('Accept', 'Application/json')
-//     .set('authorization', '')
-//     .expect(404, done);
-//       }); 
-//     });
-
-  
-//   it('Should be able to sign in and get a token - Admin', (done) => {
-//     request
-//     .post('/api/users/signin')
-//     .set('Accept', 'Application/json')
-//     .send(mockdata.adminLogin)
-//     .end((error, res) =>{
-//       adminToken = res.body.response.data.token;
-//       console.log(res)
-//       expect(res.status).to.equal(200);
-//     done();
-//       }); 
-//     });
-//  });
-
-
-
-// describe('Unathorized User' , () => {
-//  it('Should not be able to access this page', (done) => {
-//     request
-//     .post('/api/books')
-//     .set('Authorization', userToken)
-//     .send(mockdata.bookdata)
-//     .expect(401)
-//     .end((error, res) => {
-//         if(error) return done(error);
-//         done();
-//     });
-//    });
-//  });
-
-// describe('Upload books' , () => {
-//  it('Should be able to upload books', (done) => {
-//     request
-//     .post('/api/books')
-//     .set('Authorization', adminToken)
-//     .send(mockdata.bookdata)
-//     .expect(201, done);
-//    });
-// });
-
-// describe('Get books - Admin' , () => {
-//  it('Should be able to get books without signing in', (done) => {
-//     request
-//     .get('/api/books')
-//     .set('Authorization', adminToken)
-//     .expect('Content-Type', /json/)
-//     .expect(200, done);
-//    });
-// });
-
-// describe('Modify books - Admin' , () => {
-//  it('Should be able to modify books', (done) => {
-//     request
-//     .put('/api/books/1')
-//     .set('Authorization', adminToken)
-//     .send(mockdata.modifyBookData)
-//     .expect('Content-Type', /json/)
-//     .expect(200, done);
-//    });
-// });
-
-// describe('Get books - User' , () => {
-//  it('Should be able to get books without signing in', (done) => {
-//     request
-//     .get('/api/books')
-//     .set('Authorization', userToken)
-//     .expect('Content-Type', /json/)
-//     .expect(200, done);
-//    });
-// });
-
-// describe('Get books - User' , () => {
-//  it('Should be able to get books after signing in', (done) => {
-//     request
-//     .get('/api/users/:id/books')
-//     .set('Authorization', userToken)
-//     .send(mockdata.userID)
-//     .expect('Content-Type', /json/)
-//     .expect(200, done);
-//    });
-// });
-
-// describe('Borrow Books' , () => {
-//  it('Should allow users borrow books', (done) => {
-//     request
-//     .post('/api/users/:id/books')
-//     .set('Authorization', userToken)
-//     .send(mockdata.borrowBook)
-//     .expect('Content-Type', /json/)
-//     .expect(201, done);
-//    });
-// });
-
-// describe('Unauthorized Access' , () => {
-//  it('Should not allow users without token to borrow books', (done) => {
-//     request
-//     .post('/api/users/:id/books')
-//     .set('Authorization', '')
-//     .send(mockdata.borrowBook)
-//     .expect('Content-Type', /json/)
-//     .expect(401, done);
-//    });
-// });
-
-// describe('Return Books' , () => {
-//  it('Should allow users return books', (done) => {
-//     request
-//     .put('/api/users/:id/books')
-//     .set('Authorization', userToken)
-//     .send(mockdata.borrowBook)
-//     .expect('Content-Type', /json/)
-//     .expect(200, done);
-//    });
-// });
-
-// describe('Unauthorized Access' , () => {
-//  it('Should not allow users without token to return books', (done) => {
-//     request
-//     .put('/api/users/:id/books')
-//     .set('Authorization', '')
-//     .send(mockdata.borrowBook)
-//     .expect('Content-Type', /json/)
-//     .expect(401, done);
-//    });
-// });
-
-
-// describe('Borrow book' , () => {
-//  it('Should not allow users to borrow book when the body is malformatted', (done) => {
-//     request
-//     .put('/api/users/:id/books')
-//     .set('Authorization', userToken)
-//     .send(mockdata.failborrowBook)
-//     .expect('Content-Type', /json/)
-//     .expect(400, done());
-//    });
-// });
-
-// describe('Validate book details' , () => {
-//  it('Should Validate book details before create', (done) => {
-//     request
-//     .post('/api/users/:id/books')
-//     .set('Authorization', adminToken)
-//     .send(mockdata.invalidBookdata)
-//     .expect('Content-Type', /json/)
-//     .expect(400, done());
-//    });
-
-//    it('Should Validate book details before create', (done) => {
-//     request
-//     .post('/api/books')
-//     .set('Authorization', adminToken)
-//     .send(mockdata.invalidBookdata2)
-//     .expect('Content-Type', /json/)
-//     .expect(400, done());
-//    });
-
-//   it('Should Validate book details before create', (done) => {
-//     request
-//     .post('/api/books')
-//     .set('Authorization', adminToken)
-//     .send(mockdata.invalidBookdata3)
-//     .expect('Content-Type', /json/)
-//     .expect(400, done());
-//    });
+  })
 });
 
+
+//**********************************//
+//********TEST USER SIGNUP ******* //
+//**********************************//
+describe('User registration', () => {
+  it('should register a user' , (done) => {
+    request
+    .post(`${userAPI}/signup`)
+    .send('Accept', 'Application/json')
+    .send(mockdata.user1)
+    .end((err, res) => {
+      console.log(err);
+      expect(res.status).to.equal(201);
+      expect(res.body).to.have.key('responseData');
+      done();
+    });
+  });
+});
+
+
+//**********************************//
+//********TEST USER SIGNIN ******* //
+//**********************************//
+describe('User Sign in', () => {
+  it('should log a user in' , (done) => {
+    request
+    .post(`${userAPI}/signin`)
+    .send('Accept', 'Application/json')
+    .send(mockdata.user1Login)
+    .end((err, res) => {
+      expect(res.status).to.equal(200);
+      expect(res.body).to.have.key('responseData');
+      userToken =  res.body.responseData.token;
+      userId = res.body.responseData.userID;
+      done();
+    });
+  });
+});
+
+
+//**********************************//
+//********TEST USER FEATURES ******* //
+//**********************************//
+describe('User Actions', () => {
+  it('should fetch user books' , (done) => {
+    request
+    .get(`${userAPI}/${userId}/books`)
+    .set('Authorization', userToken)
+    .end((err, res) => {
+      expect(res.status).to.equal(200);
+      // expect(res.body).to.have.key('responseData');
+      done();
+    });
+  });
+
+  it('should allow user borrow a book', (done) => {
+    request
+    .post(`${userAPI}/${userId}/books`)
+    .set('Authorization', userToken)
+    .send('Accept', 'Application/json')
+    .send(mockdata.borrowBookData)
+    .end((err, res) => {
+      expect(res.status).to.equal(201);
+      expect(res.body).to.be.an('object');
+      expect(res.body).to.have.property('message');
+      expect(res.body).to.have.property('returnDate');
+      done();
+    });
+  });
+
+  it('should allow user return a book', (done) => {
+    request
+    .put(`${userAPI}/${userId}/books`)
+    .set('Authorization', userToken)
+    .send('Accept', 'Application/json')
+    .send(mockdata.borrowBookData)
+    .end((err, res) => {
+      expect(res.status).to.equal(201);
+      expect(res.body).to.be.an('object');
+      expect(res.body).to.have.property('message');
+      done();
+    })
+  });
+
+  it('should get user profile', (done) => {
+    request
+    .get(`${userAPI}/${userId}`)
+    .set('Authorization', userToken)
+    .end((err, res) => {
+      expect(res.status).to.equal(200);
+      expect(res.body).to.be.an('object');
+      expect(res.body).to.have.property('id');
+      expect(res.body).to.have.property('firstname');
+      expect(res.body).to.have.property('lastname');
+      expect(res.body).to.have.property('email');
+      expect(res.body).to.have.property('username');
+      expect(res.body).to.have.property('password');
+      expect(res.body).to.have.property('membership');
+      expect(res.body).to.have.property('createdAt');
+      expect(res.body).to.have.property('updatedAt');
+      expect(res.body).to.have.property('image');
+      done();
+    });
+  });
+
+  it('should edit user profile', (done) => {
+    request
+    .put(`${userAPI}/${userId}`)
+    .set('Authorization', userToken)
+    .send(mockdata.userEdit)
+    .end((err, res) => {
+      expect(res.status).to.equal(201);
+      expect(res.body).to.be.an('object');
+      expect(res.body.data[0]).to.have.a.property('id');
+      expect(res.body.data[0]).to.have.a.property('firstname');
+      expect(res.body.data[0]).to.have.a.property('lastname');
+      expect(res.body.data[0]).to.have.a.property('username');
+      done();
+    });
+  })
+});
+
+describe('Admin Login' , () => {
+  it('should be able to login ', (done) => {
+    request
+    .post(`${userAPI}/signin`)
+    .send('Accept', 'Application/json')
+    .send(mockdata.adminSigninData)
+    .end((err, res) => {
+      expect(res.status).to.equal(200);
+      expect(res.body).to.have.key('responseData');
+      expect(res.body.responseData).to.have.property('token');
+      expect(res.body.responseData).to.have.property('userID');
+      expect(res.body.responseData).to.have.property('username');
+      expect(res.body.responseData).to.have.property('userRole');
+      expect(res.body.responseData).to.have.property('message');
+      expect(res.body.responseData.message).to.equal('signed in');
+      adminToken = res.body.responseData.token;
+      done();
+    });
+  })
+});
+
+describe('Admin Login' , () => {
+  it('should be able to login ', (done) => {
+    request
+    .post(`${userAPI}/signin`)
+    .send('Accept', 'Application/json')
+    .send(mockdata.adminSigninData)
+    .end((err, res) => {
+      expect(res.status).to.equal(200);
+      expect(res.body).to.have.key('responseData');
+      expect(res.body.responseData).to.have.property('token');
+      expect(res.body.responseData).to.have.property('userID');
+      expect(res.body.responseData).to.have.property('username');
+      expect(res.body.responseData).to.have.property('userRole');
+      expect(res.body.responseData).to.have.property('message');
+      expect(res.body.responseData.message).to.equal('signed in');
+      adminToken = res.body.responseData.token;
+      done();
+    });
+  })
+});
+
+describe('Admin' , () => {
+  it('should upload a book', (done) => {
+    request
+    .post(`${api}/books`)
+    .set('Authorization', adminToken)
+    .send('Accept', 'Application/json')
+    .send(mockdata.bookdata)
+    .end((err, res) => {
+      expect(res.status).to.equal(201);
+      expect(res.body).to.have.property('message');
+      expect(res.body).to.have.property('data');
+      done();
+    });
+  });
+
+  it('should edit a book', (done) => {
+    request
+    .put(`${api}/books/${mockdata.editBookdata.id}`)
+    .set('Authorization', adminToken)
+    .send('Accept', 'Application/json')
+    .send(mockdata.editBookdata)
+    .end((err, res) => {
+      expect(res.status).to.equal(200);
+      expect(res.body).to.have.property('message');
+      expect(res.body.message).to.equal('Book modified successfully');
+      done();
+    });
+  });
+
+  it('should get a book', (done) => {
+    request
+    .get(`${api}/books/${mockdata.editBookdata.id}`)
+    .set('Authorization', adminToken)
+    .end((err, res) => {
+      expect(res.status).to.equal(200);
+      expect(res.body.message.id).to.equal(mockdata.editBookdata.id);
+      done();
+    })
+  })
+
+  it('should delete a book', (done) => {
+    request
+    .delete(`${api}/books/${mockdata.editBookdata.id}`)
+    .set('Authorization', adminToken)
+    .send('Accept', 'Application/json')
+    .end((err, res) => {
+      expect(res.body).to.have.property('message');
+      expect(res.body.message).to.equal('Book has been successfully deleted');
+      expect(res.body).to.have.property('updatedBooks');
+      done();
+    });
+  });
+
+  it('should restore deleted book', (done) => {
+    request
+    .post(`${api}/books/${mockdata.editBookdata.id}`)
+    .set('Authorization', adminToken)
+    .send('Accept', 'Application/json')
+    .end((err, res) => {
+      console.log(res.body)
+      // expect(res.body).to.have.property('message');
+      // expect(res.body.message).to.equal('Book has been successfully deleted');
+      // expect(res.body).to.have.property('updatedBooks');
+      done();
+    });
+  });
+});
 
 
 
