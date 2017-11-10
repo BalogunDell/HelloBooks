d041025518391addf965ea2e9c8f653bda99ad6a
Balogun Abiodun Delight <delighteddell@gmail.com>
2017-11-10 21:39:01 +0100

    bug(test): refactor travis and package.json files

--- tests/api.test.js
+++ tests/api.test.js
@@ -45,6 +45,9 @@ describe('Hellobooks API', () => {
       })
       .end((err, res) => {
         adminToken=res.body.responseData.token;
+        console.log(res, '=================');
+        console.log(res.body,'-------------body===============');
+        console.log(res.body.responseData, '=================response data');
       });
     })
     .then(() => {
