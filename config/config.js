1f80238468056571abfec5c23304154a4a29720c
Balogun Abiodun Delight <delighteddell@gmail.com>
2017-11-10 20:18:22 +0100

    bug(test): refactor travis and package.json files

--- config/config.js
+++ config/config.js
@@ -9,16 +9,7 @@ module.exports = {
     dialect: 'postgres'
   },
   test: {
-    username: 'postgres',
-    password: 'Abbey2433',
-    database: 'hellobooks_test',
-    host: '127.0.0.1',
-    dialect: 'postgres'
-  },
-
-  travis: {
-    use_env_variable: 'HELLOBOOKS_TEST',
-    dialect: 'postgres'
+    use_env_variable: 'TEST_DB'
   },
 
   production: {
