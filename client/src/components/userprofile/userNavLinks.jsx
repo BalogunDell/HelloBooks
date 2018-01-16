a6e1f9d37560320e8197093cf8b3b34cce95deba
Balogun Abiodun Delight <delighteddell@gmail.com>
2018-01-16 13:12:30 +0100

    rename files

--- client/src/components/userprofile/userNavLinks.jsx
+++
@@ -1,7 +0,0 @@
-export const userLinks = ['My Profile' , 'Book in Library', 'History', 'Borrowed Books' ,];
-export const userLinkIcons = ['person','library_books', 'history', 'book' ]
-export const userLinkText = ['profile', 'books', 'history', 'borrowedbooks' , 'notifications']
-
-export const adminLinks = ['Dashboard' , 'Books' ,'Upload Book', 'Deleted Books']
-export const adminLinkIcons = ['dashboard' , 'library_books', 'add_box' , 'library_books']
-export const adminLinkText = ['dashboard' , 'books', 'upload', 'deletedbooks']
--- client/src/utils/cloudinaryKeys.js
+++
@@ -1,3 +0,0 @@
-export const cloudinaryUrl = process.env.CLOUD_URL;
-export const cloudinaryPreset = process.env.CLOUD_KEY;
-export const requestHeader = 'application/x-www-form-urlencoded';
