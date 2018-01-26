6db55427e5592418044d8bc000d0be998d26be76
Balogun Abiodun Delight <delighteddell@gmail.com>
2018-01-26 09:26:40 +0100

    chore(readme-file): add how to contribute to readme file

--- tests/client/mocks/mockDataStorage.js
+++
@@ -1,9 +0,0 @@
- let localStorage = {
-  setItem(key, value) {
-    return Object.assign(localStorage, { [key]: value });
-  },
-  getItem(key) {
-    return localStorage[key];
-  },
-};
-export default localStorage;
